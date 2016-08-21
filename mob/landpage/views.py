6a822f7b0ead257c2b3d8b658c08294d2950d01a
Dmitry Rendov <drendov@gmail.com>
2016-08-21 17:51:06 +0000

    09 - changed landing page to CBV format

--- mob/landpage/views.py
+++ mob/landpage/views.py
@@ -1,20 +1,18 @@
 # coding=utf-8
 from django.shortcuts import render
 from django.template import RequestContext
+from django.views.generic import TemplateView
 from django.utils.translation import ugettext, ugettext_lazy as _
+
 from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 ## Only for debug
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 # end of debug
 
-@csrf_protect
-def index(request):
-    args = {}
-    return render(request, "main.html", args)
-
-@csrf_protect
-def layout(request):
-    args = {}
-    return render(request, "layout.html", args)
+@method_decorator(csrf_protect, name='dispatch')
+class LandPageView(TemplateView):
+	template_name = "main.html"
--- mob/mob/urls.py
+++ mob/mob/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.flatpages import views
-import landpage.views
 
 urlpatterns = [
-    url(r'^$', landpage.views.index, name='index'),
+    url(r'^$', include('landpage.urls', namespace='landpage')),
     url(r'^admin/', admin.site.urls),
-    url(r'^layout/', landpage.views.layout, name='layout'),
     url(r'^(?P<url>.*/)$', views.flatpage),
 ]
