e35181242dc0550aeb4a0e57617ad92d7da4f3cb
Dmitry Rendov <drendov@gmail.com>
2016-08-24 15:24:24 +0300

    09 - fixed 'view on site' button

--- mob/library/admin.py
+++ mob/library/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
+from django.urls import reverse
 
 from .models import *
 
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    view_on_site = False
 admin.site.register(Category, CategoryAdmin)
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display  = ('title', 'publish', 'status')
-    list_filter   = ('publish', 'categories', 'status')
+    list_filter   = ('publish', 'status')
     search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title',)}
+    filter_horizontal = ('categories',)
+    def view_on_site(self, obj):
+        url = reverse('library:article_detail', kwargs={'slug': obj.slug})
+        return url
+
 admin.site.register(Article, ArticleAdmin)
