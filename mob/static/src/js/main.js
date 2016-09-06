a576c4609e188d386bf125870ce06a4f3eb0385c
Dmitry Rendov <drendov@gmail.com>
2016-09-06 16:43:55 +0300

    11 - hidden email from search engines

--- mob/static/src/js/main.js
+++ mob/static/src/js/main.js
@@ -1,5 +1,5 @@
-//jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
+
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
@@ -7,7 +7,15 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
+
     $('img').error(function(){
 		$(this).attr('src', '/static/img/image-not-found.png');
 	});
+
+    var e = "info";
+    var t = "minecraft";
+    var n = ".of.by";
+    var r = e + '@' + t + n;
+    $(".hide-email").attr('href','mailto:' + r).html(r);
+
 }); 
